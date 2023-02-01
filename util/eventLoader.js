e3ab228b153e0383514e122d3e0465eff1dd015c
Ayberk DEMİR <77262747+AtaGalata@users.noreply.github.com>
2023-02-01 13:54:43 +0300

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,6 +1,24 @@
-const reqEvent = event => require(`../events/${event}`);
+const reqEvent = event => require(`../events/${event}`); //Event Klasörünü Belirtiyoruz
+// const verilcek_isim = event => require(`yol/${event}`) // şeklinde yazılacaktır bu yazılana göre de aşağıya eklenecektir.
 
 module.exports = (client) => {
     client.on("ready", () => reqEvent("ready")(client));
     client.on("messageCreate", reqEvent("komutlar"));
+    
+    /*
+    client.on(işlem, () => verilecek_isim("dosyaadı"))
+    dosya yolunu buldurduk ve gitti şimdi dosyanın geri yanıt vermesi lazım
+    
+   dosyaya;
+   
+   module.exports = (içe_aktarılan_işlem) => {}
+   
+   burda dikkat edilmesi gereken içe_aktarılan_işlem ile işlem aynı değildir
+   
+   örnek olarak;
+   
+   client.on("ready", r => {}) de
+   ready : işlem
+   r ise içe_aktarılan_işlemdir
+    */
 }
